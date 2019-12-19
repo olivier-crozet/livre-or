274e5d36fe58ecc2c1e0cec0d3803c5774ba452c
olivier-crozet <olivier.crozet@laplateforme.io>
2019-12-19 01:18:55 +0100

    VERSION portefolio

--- index.php
+++ index.php
@@ -1,43 +1,90 @@
 <!DOCTYPE html>
 <html>
-
 <head>
-    <title>livre-or</title>
-    <meta sharset="utf-8">
-    <link rel="stylesheet" href="index.css">
-    </head>
-
-        
-<body>
-    <header>
-        <nav>
-            <ul>
-                <li><a href="connexion.php">Connexion</a></li>
-                <li><a href="inscription.php">Inscription</a></li>
-                <li><a href="livre-or.php">Livre d'Or</a></li>
-            </ul>
-        </nav>
-    </header>
-
-<div class="align">  
-    <p><strong> Bienvenue sur </p><p class="nom"> "PROTECT ANIMAL"</strong></p>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="css/nav.css">
+	<link rel="stylesheet" type="text/css" href="css/index.css">
+	<title>porte folio</title>
+</head>
+	<body>
+<header>
+		  <nav class="oc-header">
+       <ul class="menu">
+  
+  <li class="oc-onglet"><a class="oc-nav-marg" href="voyage.html">accueil</a> </li>
+  
+  <li class="oc-onglet" class="violet"><a class="oc-nav-marg" href="#">languages</a>
+        <ul class="oc-back1">
+  <li class="oc-sous"><a href="#">html</a>
+    <ul>
+  <li><a class="oc-sous-sous" href="voyage21.html">404</a></li>
+  <li><a class="oc-sous-sous" href="voyage22.html">Aplication favorite</a></li>
+        </ul>
+        </li>
+  <li>
+  <li class="oc-sous"><a href="#">php.sql</a>
+    <ul>
+  <li><a class="oc-sous-sous" href="voyage11.html">connexion</a></li>
+  <li><a class="oc-sous-sous" href="voyage12.html">discussion</a></li>
+     </ul>
+  </li>
+  <li class="oc-sous"><a href="#">olivier</a>
+     <ul>
+  <li><a class="oc-sous-sous" href="voyage31.html">la dominique</a></li>
+  <li><a class="oc-sous-sous" href="voyage32.html">turquie</a></li>
+    </ul>
+    </ul>
+  </li>
+  <li>
+  <li class="oc-onglet"><a class="oc-nav-marg" href="#">livre d'or</a>
+    <ul class="oc-back1">
+ <!-- <li class="oc-sous"><a href="voyage21.html">norvège</li></a>
+  <li class="oc-sous"><a href="voyage22.html">bénin</li></a>
+  <li class="oc-sous"><a href="voyage11.html">brésil</li></a>
+  <li class="oc-sous"><a href="voyage12.html">japon</li></a>
+  <li class="oc-sous"><a href="voyage31.html">la dominique</li></a>
+  <li class="oc-sous"><a href="voyage32.html">turquie</li></a>-->
+  <li></li>
+      </ul>
+  </li>
+  </li> 
+  <li class="oc-onglet"><a class="oc-nav-marg" href="voyage2.html">C.V</li></a>
+  <li class="oc-onglet"><a class="oc-nav-marg" href="voyage2.html">contact</li></a>
+  </ul>
+  </nav>	
+</header>
+  <!--FIN DU HEADER -->
+        <h1 class="oc-index-titre1">Porte folio</h1>
+<div class="oc-div-background-index" >
+  <section class="oc-presentation-index">
+           <article class="oc-article-nomprenom-index">
+                   <h2>Crozet olivier</h2>
+                   <p>Developpeur web</p>
+                   <p>inovant, polyvalent et humain!</p>
+           </article class="oc-article-devise-index">
+           <article class="oc-devise-index">
+                   <h2>Echantillon des project realisé en 2019</h2>
+                   <p>Se port folio comporte mes projets realisé durant ma formation a la plateforme durant l'année 2019.<br>
+                   j'ai commencé il y a un an avec un niveau zero en informatique, vous pouvez voir mon evolution en regardent les projet developpé avec la plateforme et vous faire une idées si vous etes un employeur,un partenaire potentiel, ou un future candidant à la plateforme !</p>
+           </article>
+           <article class="oc-article-monimage-index">
+                   <img class="oc-monimage-index" src="image/olivier.crozet.jpg">
+           </article>
+  </section>
 </div>
-
-    <div id ="bonne">
-         <p> Nous vous souhaitons une bonne navigation... </p>
-    </div>
-    
-            <form action="connexion.php" metod="POST"></form>
-            <form action="inscription.php" metod="POST"></form>
-            <form action="livre-or.php" metod="POST"></form>
-
-
-        <footer id="logo">
-    
-            <img height="60" src="footeror.webp">
-                
-        </footer>
-
-</body>
-
-            
+                      <!--partie php-->
+                      <h1 class="oc-titre-php-index">PHP</h1>
+  <section class="oc-section2-index">                 
+           <article class="oc-article-connexion-php" >
+                    <img class="image-connexion-php" src="image/connexion.png">
+           </article>
+           <article class="oc-article-discussion-php">
+                    <img class="image-discussion-php" src="image/discussion.png">
+           </article>
+  </section>
+  <section>
+                   <h1 class="oc-titre-html">HTML/CSS</h1>
+           <article>
+                   <img class="">
+           </article>
+  </section>
